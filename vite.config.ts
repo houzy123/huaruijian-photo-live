967d9d8c41e6e6312b0272f37a5cdd8309705e07
houzy123 <385109219@qq.com>
2026-07-09 01:58:29 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'plugin-inspect-react-code'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/huaruijian-photo-live/',
   plugins: [inspectAttr(), react()],
   server: {
     port: 3000,
